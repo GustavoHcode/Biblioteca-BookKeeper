ac00dbc5fca9b4eea055ec8122017adebbec1fcb
GustaDP <gustavodpaula3105@gmail.com>
2024-05-26 10:14:06 -0300

    corrigido o bug encontrado ao puxar do git

--- biblioteca_app/urls.py
+++ biblioteca_app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 from .views import home, save_student, readers, books, save_readers, add_book, remove_book, edit_book_list_view, edit_book_view, loan_books, view_loans
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/home/', permanent=False), name='index'),
     path('home/', home, name='home'),
     path('save/', save_student, name='save_student'),
     path('readers/', readers, name='readers'),
@@ -14,5 +16,4 @@ urlpatterns = [
     path('editbook/<int:pk>/', edit_book_view, name='edit_book'),
     path('loan_books/', loan_books, name='loan_books'),
     path('returns/', view_loans, name='returns'),
-
 ]
